bf3d6ed916a68a37ea75c145202fb8f9c1053d19
Lucas Magalhães <lucas.castro@santodigital.com.br>
2023-07-20 20:00:18 -0300

    Ajuste do mount

--- backend/core/database.py
+++ backend/core/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 #SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://db_admin:senha_forte@db-usuario/usuario"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://db_admin:senha_forte@db-usuario/usuario"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
