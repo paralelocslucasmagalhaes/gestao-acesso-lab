80137963f2fec6f3f17cbef55e7b48d515005c26
Lucas Magalhães <lucas.castro@santodigital.com.br>
2023-08-03 20:34:17 -0300

    dns

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    api: 'http://api-service-gestaoacesso/api/v1'
+    api: 'https://api-service-gestaoacesso.gestao-acesso.svc.cluster.local/api/v1'
   };
   
